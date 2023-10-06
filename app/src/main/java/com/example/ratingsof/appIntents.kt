36dfd3ccdb4b66f4923eaef05ffe36be19831e4e
vane <jvacosta29@misena.edu.co>
2023-10-06 14:14:54 -0500

    nuevp

--- app/src/main/java/com/example/ratingsof/appIntents.kt
+++ app/src/main/java/com/example/ratingsof/appIntents.kt
@@ -3,6 +3,7 @@ package com.example.ratingsof
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 
 class appIntents : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +13,39 @@ class appIntents : AppCompatActivity() {
         val textView1 = findViewById<TextView>(R.id.vista_1)
         val receivedMessage:String = intent.extras?.getString("EXTRA_MASAGGE").orEmpty()
         textView1.text = receivedMessage
+        Toast.makeText(this, "metodo Oncreate",Toast.LENGTH_LONG).show()
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        Toast.makeText(this, "metodo Oncreate",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "metodo onResume",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "metodo onStop",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "metodo onPause",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "metodo onRestart",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "metodo onDestroy",Toast.LENGTH_LONG).show()
+    }
+
 }
+
